e8913c6fab7106a413b8deb4dc34d73ca73a97b3
tp <tp@octavore.com>
2017-04-04 23:03:46 -0700

    Fix import path.

--- protoc-gen-go-2/main.go
+++ protoc-gen-go-2/main.go
@@ -53,7 +53,7 @@ import (
 	"os"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/protoc-gen-go/generator"
+	"github.com/octavore/protobuf/protoc-gen-go/generator"
 )
 
 func main() {
